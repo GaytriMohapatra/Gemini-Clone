1d438e7f11e372b13f7bbf5ba479fc45860c60ff
Gaytri Mohapatra <gaytrimoh@gmail.com>
2024-04-14 15:24:48 +0530

    LFS

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,90 @@
+import { useContext} from 'react'
+import './Main.css'
+import { assets } from '../../assets/assets'
+import { Context } from '../../context/Context'
+
+const Main = () => {
+
+  const { onSent,
+    recentPrompt,
+    showResult,
+    loading,
+    resultData,
+    setInput,
+    input
+  } = useContext(Context);
+
+
+  return (
+    <div className='main'>
+      <div className="nav">
+        <p>Gemini</p>
+        <img src={assets.user_icon} alt="" />
+      </div>
+      <div className="main-container">
+        {showResult
+          ? <div className="result">
+            <div className='result-title'>
+              <img src={assets.user_icon} alt="" />
+              <p>{recentPrompt}</p>
+            </div>
+            <div className="result-data">
+              <img src={assets.gemini_icon} alt="" />
+              {loading
+                ? <div className="loader">
+                  <hr className="animated-bg" />
+                  <hr className="animated-bg" />
+                  <hr className="animated-bg" />
+                </div>
+                : <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+              }
+            </div>
+
+          </div>
+          : <>
+            <div className="greet">
+              <p><span>Hello, Techies!</span></p>
+              <p>How can I help you today?</p>
+            </div>
+            <div className="cards">
+              <div className="card">
+                <p>Debug this code and suggest a fix (provide the code with the error).</p>
+                <img src={assets.compass_icon} alt="" />
+              </div>
+              <div className="card">
+                <p>Describe how blockchain technology works in simple language.</p>
+                <img src={assets.bulb_icon} alt="" />
+              </div>
+              <div className="card">
+                <p>Write a code snippet that makes your computer sing a song.</p>
+                <img src={assets.message_icon} alt="" />
+              </div>
+              <div className="card">
+                <p>Create a funny tech support conversation between a human and a chatbot.</p>
+                <img src={assets.code_icon} alt="" />
+              </div>
+            </div>
+          </>
+        }
+
+
+
+        <div className="main-bottom">
+          <div className="search-box">
+            <input onChange={(e) => setInput(e.target.value)} value={input} type="text" placeholder='Enter a prompt here' />
+            <div>
+              <img src={assets.gallery_icon} width={30} alt="" />
+              <img src={assets.mic_icon} width={30} alt="" />
+              {input ? <img onClick={() => onSent()} src={assets.send_icon} width={30} alt="" /> : null}
+            </div>
+          </div>
+          <p className="bottom-info">
+            Gemini may display inaccurate info, including about people, so double-check its responses. Your privacy and Gemini Apps
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Main
